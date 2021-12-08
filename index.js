513cb4a3e5bd0c426cfe41f951c27c0cd04a4468
annecie <ajbexcelsior@gmail.com>
2021-12-08 15:55:35 +0000

    completed

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-//write your code here
+//question 1
+const animals = ['dog', 'cat', 'bear'];
+const shortWords = (array) => {
+    return array.filter(el => el.length < 5);
+};
+console.log(shortWords(animals));
+//question 2
+const noVowel = (array) => {
+  return array.map(word=> word.replace(/[aeiou]/gi, "x"));  
+};
+console.log(shortWords(animals));
+//question 3
+const longToShort = (array) => {
+  return array.sort((a, b) => a.length - b.length);   
+};
+//question 4
+const onlyVowelA = (array) => {
+    return array.filter(element => element.includes("a"));
+};
+//question 5
+const pluralize = (array) => {
+    return array.map(element => element + "s")    
+};
+// question 6
+const longerThanSeven = (array) => {
+    return array.some( word => word.length > 7);
+}
+console.log(longerThanSeven(animals));
+// question 7
+const oddlength = (array) => {
+    
+};
+const allFour = (array) => {
+    return array.some(word => word.length === 4);
+};
+console.log(allFour(animals));
+//question 12
+const concatStrings = (array) => {
+    return array.reduce((prev, current) => `${prev} ${current}`)
+};
+console.log(concatStrings(animals));
